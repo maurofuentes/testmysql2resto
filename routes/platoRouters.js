22730a36a303ae07acc3f7813a4e3e0d00c30357
Mauro Fuentes <maurofuentes983@gmail.com>
2020-11-15 18:02:51 -0300

    categoriasController & platosController renamed

--- routes/platoRouters.js
+++ routes/platoRouters.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const platosController = require("../controllers/platos.controller");
+const platosController = require("../controllers/platosController");
 
 router.get("/", platosController.getPlatos);
 
